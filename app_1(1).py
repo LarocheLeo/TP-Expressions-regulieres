c88f6dfe7b2991f74d1b70f601036e12d30ce9c9
LarocheLeo <larocheleo@free.fr>
2025-12-09 11:12:39 +0100

    tp terminer

--- app_1(1).py
+++ app_1(1).py
@@ -1,29 +1,39 @@
-from flask import Flask, render_template
-import mysql.connector
+from flask import Flask, render_template, request
+import re
 
 app = Flask(__name__)
- 
-# MySQL configuration
-db_config = {
-    'host': 'tp4-sql',
-    'user': 'root',
-    'password': 'foo',
-    'database': 'demosql'
-}
-
-# Initialize MySQL connection
-conn = mysql.connector.connect(**db_config)
-cursor = conn.cursor() 
-
 
 @app.route('/')
 def index():
-    # Sample query
-    query = "SELECT * FROM myTable"
-    cursor.execute(query)
-    data = cursor.fetchall()
+    return "Page d'accueil"
 
-    return render_template('index.html', data=data)
+@app.route('/newuser/', methods=['GET', 'POST'])
+def newuser():
+    message = ""
+    if request.method == 'POST':
+        username = request.form.get('username', '')
+
+        # Critères
+        criteres = [
+            (r'.{6,}', "Au moins 6 caractères"),
+            (r'.*[0-9].*', "Au moins 1 chiffre"),
+            (r'.*[A-Z].*', "Au moins 1 majuscule"),
+            (r'.*[a-z].*', "Au moins 1 minuscule"),
+            (r'.*[#%{}@].*', "Au moins 1 caractère spécial parmi #%{}@")
+        ]
+
+        # Vérification
+        erreurs = []
+        for patt, msg in criteres:
+            if re.fullmatch(patt, username) is None and not re.search(patt, username):
+                erreurs.append(msg)
+
+        if not erreurs:
+            message = "Identifiant valide ✅"
+        else:
+            message = "Échec sur : " + ", ".join(erreurs)
+
+    return render_template('newuser.html', message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
